3fc9003e9cf01074d8123e5a0fce740edbb1c17e
Thaddeus <thaddeuspearson@gmail.com>
2025-02-25 23:46:29 +0100

    finish dms.py

--- Small_Problems/Easy_2/dms.py
+++ Small_Problems/Easy_2/dms.py
@@ -9,13 +9,35 @@ seconds in a minute.
 import sys
 
 
-def dms():
-    pass
+DEGREE = "\u00B0"
+
+
+def dms(angle=None):
+    """Returns the degrees, minutes, and seconds of the given angle.
+
+    :param angle (float): the angle
+    :returns (str): formatted string giving degree, minutes, and seconds
+    """
+    if angle is None:
+        angle = input("Enter a float representing the degree of an angle: ")
+
+    minute = (angle - int(angle)) * 60
+    second = (minute - int(minute)) * 60
+    return f"{int(angle)}{DEGREE}{int(minute):02}'{int(second):02}\""
 
 
 # pylint: disable=pointless-string-statement
-"""CODE EXPLAINATION
+"""CODE EXPLANATION
+dms is defined on line 15, and accepts a single parameter, angle. The
+conditional block on line 21 checks to see if angle has an argument provided,
+and if not, promts the user to input one.
 
+On line 24, the local variable minute is instantiaed to the decimal portion
+of angle and multiploied by 60. On line 25, a local variable second is
+instantiated to the decimal portion of minute and multiplied by 60.
+
+On line 26, the properly formatted string is returned using string
+interpolation with padding where necessary.
 """
 
 
@@ -30,4 +52,4 @@ if __name__ == "__main__":
         assert dms(0) == "0°00'00\""
         assert dms(360) == "360°00'00\"" or dms(360) == "0°00'00\""
     else:
-        print(dms()) 
+        print(dms())

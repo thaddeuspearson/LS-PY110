c5c382183045349cddb20e0e2c365ff6b729ca03
Thaddeus <thaddeuspearson@gmail.com>
2025-02-26 00:04:28 +0100

    typo fixes union.py

--- Small_Problems/Easy_2/union.py
+++ Small_Problems/Easy_2/union.py
@@ -7,10 +7,11 @@ import sys
 
 
 def union(list_1, list_2):
-    """Returns the deduplicated union of 2 lists
+    """Returns the union of 2 lists as a set
 
     :param list_1 (list<multi>): first list to union
     :param list_2 (list<multi>): second list to union
+    :returns (set): the unioned set
     """
     return set(list_1) | set(list_2)
 
@@ -19,8 +20,8 @@ def union(list_1, list_2):
 """CODE EXPLANATION
 union is defined on line 9 and has 2 parameters, list_1 and list_2.
 
-On line 15, bot lists are cast to sets, and unioned with the union operator.
-the resulting unioned sed
+On line 15, both lists are cast to sets, and unioned with the union operator.
+The resulting unioned set from this operation is then returned.
 """
 
 
@@ -29,5 +30,3 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     if args and args[0] == "test":
         assert union([1, 3, 5], [3, 6, 9]) == {1, 3, 5, 6, 9}
-    else:
-        print()

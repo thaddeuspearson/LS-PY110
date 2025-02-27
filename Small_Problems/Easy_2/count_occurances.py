0eabc4e8c2cb917f2e47c7f6aaf6dfa81e079180
Thaddeus <thaddeuspearson@gmail.com>
2025-02-27 23:59:59 +0100

    add count_occurances.py initial setup

---
+++ Small_Problems/Easy_2/count_occurances.py
@@ -0,0 +1,25 @@
+"""
+Write a function that counts the number of occurrences of each element in a
+given list. Once counted, print each element alongside the number of
+occurrences. Consider the words case sensitive e.g. ("suv" != "SUV").
+"""
+import sys
+
+
+def count_occurances():
+    return
+
+
+# pylint: disable=pointless-string-statement
+"""CODE EXPLANATION
+
+"""
+
+
+# Test Cases
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args and args[0] == "test":
+        assert 
+    else:
+        print()

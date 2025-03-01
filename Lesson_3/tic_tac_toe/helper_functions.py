2df43be5508da28aa5a4f56533dfe2033f036c4f
Thaddeus <thaddeuspearson@gmail.com>
2025-03-01 23:55:24 +0100

    rf: kwargs>valid_choices list, helper_functions.py

--- Lesson_3/tic_tac_toe/helper_functions.py
+++ Lesson_3/tic_tac_toe/helper_functions.py
@@ -33,27 +33,29 @@ def prompt(message: str) -> None:
     print(f"==> {message}")
 
 
-def is_valid_choice(choice: str, **kwargs: dict) -> bool:
+def is_valid_choice(choice: str, valid_choices: list) -> bool:
     """Validates if a given choice (in string format) is in the
-    given valid_operations (passed into kwargs).
+    given valid_choices (passed into kwargs).
 
-    :num_str (str): a string representation of a number
+    :choice (str): the player choice
+    :valid_choices (list<int>): a list of valid choices
     :returns (bool): True / False
     """
-    return choice in kwargs.get("valid_choices", [])
+    return int(choice) in valid_choices
 
 
 def get_valid_user_input(message: str, validation_func: Callable,
-                         **kwargs: dict) -> str:
+                         valid_choices: list) -> str:
     """Gets user input, validated by the given validation function.
 
     :message (str): The message to prompt the user
     :validation_func (Callable): the function to validate the user input
+    :valid_choices (list): the list of valid choices for the validation func
     :returns user_input (str): The validated user input string
     """
     prompt(message)
     user_input = input()
-    while not validation_func(user_input, **kwargs):
+    while not validation_func(user_input, valid_choices):
         prompt(f"Invalid entry. {message}")
         user_input = input()
     return user_input

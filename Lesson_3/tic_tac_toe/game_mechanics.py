c821277d4fdd39d0967a1dc070dfdc8c706dbb84
Thaddeus <thaddeuspearson@gmail.com>
2025-03-03 00:46:23 +0100

    make computer defensive

--- Lesson_3/tic_tac_toe/game_mechanics.py
+++ Lesson_3/tic_tac_toe/game_mechanics.py
@@ -3,10 +3,13 @@ from random import choice
 from helper_functions import get_valid_user_input, prompt
 
 
-SLOT_PLACEHOLDER = " "
+SQUARE_PLACEHOLDER = " "
+WIN_LINES = [('1', '2', '3'), ('4', '5', '6'), ('7', '8', '9'),
+             ('1', '4', '7'), ('2', '5', '8'), ('3', '6', '9'),
+             ('1', '5', '9'), ('3', '5', '7')]
 
 
-def display_board(slots: dict = {}) -> None:
+def display_gameboard(squares: dict = {}) -> None:
     """Prints a tic tac toe board with current player positions.
 
     :param positions (dict): player and computer positions
@@ -14,11 +17,11 @@ def display_board(slots: dict = {}) -> None:
     """
     board = (
         f"\n"
-        f" {slots.get('1')} │ {slots.get('2')} │ {slots.get('3')}"
+        f" {squares.get('1')} │ {squares.get('2')} │ {squares.get('3')}"
         f"\n───┼───┼───\n"
-        f" {slots.get('4')} │ {slots.get('5')} │ {slots.get('6')}"
+        f" {squares.get('4')} │ {squares.get('5')} │ {squares.get('6')}"
         f"\n───┼───┼───\n"
-        f" {slots.get('7')} │ {slots.get('8')} │ {slots.get('9')}\n")
+        f" {squares.get('7')} │ {squares.get('8')} │ {squares.get('9')}\n")
     print(board)
 
 
@@ -30,12 +33,12 @@ def initialize_gameboard() -> dict:
     return {str(square): " " for square in range(1, 10)}
 
 
-def process_gameboard(player: str = None, gameboard: dict = None,
+def process_gameboard(marker: str = None, gameboard: dict = None,
                       choice: str = None) -> list:
     """Processes the gameboard with the most recent choice. Initializes the
     gameboard if called without an argument for gameboard.
 
-    :param player(str): marker of the player who made the choice
+    :param marker (str): marker of the player who made the choice
     :gameboard (dict): the current game state
     :param choice (str): the square choosen
     :returns (list): the remaining available positions
@@ -44,10 +47,19 @@ def process_gameboard(player: str = None, gameboard: dict = None,
     if not gameboard:
         gameboard = initialize_gameboard()
     if choice:
-        gameboard[choice] = player
-    display_board(gameboard)
+        gameboard[choice] = marker
+    display_gameboard(gameboard)
     valid_positions = get_valid_positions(gameboard)
     return gameboard, valid_positions
+
+
+def get_current_gameboard_lines(gameboard: dict) -> list:
+    """Gets the lines of the gameboard at its current state.
+
+    :param gameboard (dict): the current state of the gameboard
+    :returns (list<tuple>): the current lines of the gameboard
+    """
+    return [[gameboard[square] for square in line] for line in WIN_LINES]
 
 
 def set_markers(message: str) -> str:
@@ -75,15 +87,36 @@ def get_valid_positions(gameboard: dict) -> list:
     :param gameboard (dict): the current state of the gameboard
     :returns (list): the available positions
     """
-    return [slot for slot in gameboard if gameboard[slot] == SLOT_PLACEHOLDER]
+    return [square for square in gameboard
+            if gameboard[square] == SQUARE_PLACEHOLDER]
 
 
-def claim_square(player, message, valid_positions):
+def get_squares_at_risk(computer_marker: str, gameboard: dict) -> list:
+    """Gets any squares at risk of being completed by player.
+
+    :param computer_marker (str): the marker for the current player
+    :param gameboard (dict): the current state of the gameboard
+    :returns squares_at_risk (list<str>): the squares at risk
+    """
+    current_lines = get_current_gameboard_lines(gameboard)
+    opponent_marker = "X" if computer_marker == "O" else "O"
+    squares_at_risk = []
+
+    for line, square in zip(current_lines, WIN_LINES):
+        if line.count(opponent_marker) == 2 and SQUARE_PLACEHOLDER in line:
+            squares_at_risk.append(square[line.index(SQUARE_PLACEHOLDER)])
+    return squares_at_risk
+
+
+def claim_square(player: str, marker: str, message: str,
+                 gameboard: dict, valid_positions: list):
     """Claims a square for the given player.
 
-    :param: player (str): the current player
-    :param prompt (str): the message to display
-    :valid_positions (list): the availabile positions to choose from
+    :param player (str): the player for the current turn
+    :param marker (str): the marker for the current player
+    :param message (str): the message to prompt or display
+    :param gameboard (dict): the current state of the gameboard
+    :param valid_positions (list): the list of current valid positions
     :returns choice (str): the number of the square claimed"""
     match player:
         case "player":
@@ -91,7 +124,11 @@ def claim_square(player, message, valid_positions):
         case "computer":
             prompt(message)
             input()
-            choice = choose_random_valid_position(valid_positions)
+            squares_at_risk = get_squares_at_risk(marker, gameboard)
+            if squares_at_risk:
+                choice = choose_random_valid_position(squares_at_risk)
+            else:
+                choice = choose_random_valid_position(valid_positions)
     return choice
 
 
@@ -99,13 +136,14 @@ def process_turn(player: str, marker: str, message: str,
                  gameboard: dict, valid_positions: list) -> list:
     """Processes a turn by updating the board with the players choice.
 
-    :param player (str): the player's marker for the current turn
+    :param player (str): the player for the current turn
+    :param marker (str): the marker for the current player
     :param message (str): the message to prompt or display
     :param gameboard (dict): the current state of the gameboard
     :param valid_positions (list): the list of current valid positions
     :returns valid_positions (list): the updated positions on the board
     """
-    choice = claim_square(player, message, valid_positions)
+    choice = claim_square(player, marker, message, gameboard, valid_positions)
     gameboard, valid_positions = process_gameboard(marker, gameboard, choice)
     return valid_positions
 
@@ -116,15 +154,11 @@ def check_for_winner(gameboard: dict) -> bool:
     :param gameboard (dict): the current state of the game
     :returns (str | None): the winning marker ('X' | 'O') or None
     """
-    win_lines = [('1', '2', '3'), ('4', '5', '6'), ('7', '8', '9'),
-                 ('1', '4', '7'), ('2', '5', '8'), ('3', '6', '9'),
-                 ('1', '5', '9'), ('3', '5', '7')]
+    current_lines = get_current_gameboard_lines(gameboard)
 
-    curr_lines = [[gameboard[square] for square in line] for line in win_lines]
-
-    for line in curr_lines:
+    for line in current_lines:
         line = set(line)
-        if len(line) == 1 and SLOT_PLACEHOLDER not in line:
+        if len(line) == 1 and SQUARE_PLACEHOLDER not in line:
             return line.pop()
     return None
 

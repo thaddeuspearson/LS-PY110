847aa58af91b52a6730e2eb2f00440107040a24c
Thaddeus <thaddeuspearson@gmail.com>
2025-03-05 02:00:53 +0100

    WIP: finished through initial deal

--- Lesson_3/twenty_one/twenty_one.py
+++ Lesson_3/twenty_one/twenty_one.py
@@ -6,6 +6,9 @@ from sys import path
 from pathlib import Path
 path.append(str(Path(__file__).resolve().parent / '../utils/'))
 from helper_functions import get_message_dict, prompt
+from game_mechanics import total, deal
+from classes import Deck, Hand
+
 
 # Constants
 MESSAGES_PATH = "./game_messages.json"
@@ -15,6 +18,11 @@ def main():
     """Main driver function for the game"""
     messages = get_message_dict(MESSAGES_PATH)
     prompt(messages["welcome"])
+    deck = Deck()
+    player_hand = Hand("Player")
+    dealer_hand = Hand("Dealer")
+    hands = [player_hand, dealer_hand]
+    deal(deck, hands)
 
 
 if __name__ == "__main__":
